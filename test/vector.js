455a7946d46697e518b78a350a8da8e462f25693
Anders Rune Jensen <arj03@protonmail.ch>
2021-06-15 09:21:42 +0200

    Update to use test vector from spec

--- test/vector.js
+++ test/vector.js
@@ -3,18 +3,15 @@ const fs = require('fs')
 const mfff = require('../')
 const bfe = require('../ssb-bfe')
 
-const vec = JSON.parse(fs.readFileSync('testvector-metafeed-managment.json', 'utf8'))
+const vec = JSON.parse(fs.readFileSync('test/testvector-metafeed-managment.json', 'utf8'))
 
 tape('vector', function(t) {
-  // FIXME: must be included in each entry
-  const author = vec.Entries[0].HighlevelContent[0].metafeed
-
   vec.Entries.forEach(msg => {
     if (msg.HighlevelContent[0].nonce)
       msg.HighlevelContent[0].nonce = Buffer.from(msg.HighlevelContent[0].nonce, 'base64')
-    
+
     const msgExtracted = {
-      author,
+      author: msg.Author,
       sequence: msg.Sequence,
       previous: msg.Previous,
       timestamp: msg.Timestamp,
