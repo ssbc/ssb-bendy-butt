12157c6d4bbcdb26cee20fdc75e612fae1834cb4
Andre Staltz <andre@staltz.com>
2021-07-15 16:23:55 +0300

    refactor test/vector.js for clarity

--- test/vector.js
+++ test/vector.js
@@ -1,42 +1,42 @@
 const tape = require('tape')
 const fs = require('fs')
-const bb = require('../')
 const bfe = require('ssb-bfe')
+const bb = require('../')
 
 const vec = JSON.parse(
   fs.readFileSync('test/testvector-metafeed-managment.json', 'utf8')
 )
 
-tape('vector', function (t) {
-  vec.Entries.forEach((msg, i) => {
-    if (msg.HighlevelContent[0].nonce)
-      msg.HighlevelContent[0].nonce = Buffer.from(
-        msg.HighlevelContent[0].nonce,
-        'base64'
-      )
+function entryToMsgValue(entry) {
+  const { Author, Sequence, Previous, Timestamp, Signature } = entry
+  let [content, contentSignature] = entry.HighlevelContent
+  if (typeof content.nonce === 'string') {
+    content.nonce = Buffer.from(content.nonce, 'base64')
+  }
+  contentSignature = bfe.decode(Buffer.from(contentSignature.HexString, 'hex'))
+  const signature = bfe.decode(Buffer.from(Signature, 'hex'))
 
-    const msgExtracted = {
-      author: msg.Author,
-      sequence: msg.Sequence,
-      previous: msg.Previous,
-      timestamp: msg.Timestamp,
-      content: msg.HighlevelContent[0],
-      contentSignature: bfe.decode(
-        Buffer.from(msg.HighlevelContent[1].HexString, 'hex')
-      ),
-      signature: bfe.decode(Buffer.from(msg.Signature, 'hex')),
-    }
+  return {
+    author: Author,
+    sequence: Sequence,
+    previous: Previous,
+    timestamp: Timestamp,
+    content,
+    contentSignature,
+    signature,
+  }
+}
+
+tape('vector', function (t) {
+  vec.Entries.forEach((entry) => {
+    const msgExtracted = entryToMsgValue(entry)
     const encoded = bb.encode(msgExtracted)
 
-    const jsonEncodeDecode = JSON.stringify(bb.decode(encoded), null, 2)
-    const decode = JSON.stringify(
-      bb.decode(Buffer.from(msg.EncodedData, 'hex')),
-      null,
-      2
-    )
+    const jsonEncodeDecode = bb.decode(encoded)
+    const decode = bb.decode(Buffer.from(entry.EncodedData, 'hex'))
 
     t.deepEqual(jsonEncodeDecode, decode, 'decode work')
-    t.deepEqual(encoded.toString('hex'), msg.EncodedData, 'encode work')
+    t.deepEqual(encoded.toString('hex'), entry.EncodedData, 'encode work')
   })
 
   t.end()
